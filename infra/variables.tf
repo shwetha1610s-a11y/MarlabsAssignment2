806987ac02df3e27331172a8de68ee0927af7b3e
shwetha1610s-a11y <shwetha1610s@gmail.com>
2025-08-24 10:15:52 +0530

    Create  variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "admin_username" {}
+variable "admin_password" {}
+variable "tenant_id" {}
+variable "dev_user_object_id" {}
+variable "tags" { type = map(string) }
